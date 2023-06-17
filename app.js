85d984f15391916fa60c25cb19bd6d7e8e7325d7
hoquocbaoproton <hoquocbao@protonmail.com>
2023-06-17 23:11:26 +0700

    delete svelte

--- app.js
+++ app.js
@@ -10,26 +10,22 @@ const commentRoutes = require('./routes/commentRoutes');
 
 const insertDataRoutes = require('./routes/insertDataRoutes');
 
-const router = express.Router();
-
-const route = router.get('/', (req, res) => {
-  res.send('Hello world');
-});
-
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'svelte', 'public')));
 
 // 3) routes
-app.use('/', route);
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
-
 app.use('/api/insertData', insertDataRoutes);
 
 app.use(globalErrorHandler);
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 module.exports = app;
--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`App running on ${port}...`);
 });
