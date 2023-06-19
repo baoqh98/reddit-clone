25c60a19edec173445df09c96c77e90c4bfab870
hoquocbaoproton <hoquocbao@protonmail.com>
2023-06-19 11:52:23 +0700

    UI: Post

--- svelte/src/pages/Home.svelte
+++ svelte/src/pages/Home.svelte
@@ -6,7 +6,7 @@
   <div class="grid grid-cols-3 gap-4">
     <div class="col-span-2"><Post /></div>
     <div>
-      <div class="card p-4">card left</div>
+      <div class="card p-4">card right</div>
     </div>
   </div>
 </div>
