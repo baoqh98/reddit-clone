7a3537822582b9828898a9fc70023c796e396da2
Quoc Bao <hoquocbao@protonmail.com>
2023-07-26 14:32:55 +0700

    Get data in route: user/:username

--- svelte/src/routes/user/[slug]/+page.svelte
+++ svelte/src/routes/user/[slug]/+page.svelte
@@ -1,42 +1,13 @@
 <script>
   /** @type {import('./$types').PageData} */
   import MyPostList from '../../../components/User/MyPostList.svelte';
-
   import UserInfo from '../../../components/User/UserInfo.svelte';
 
   export let data;
   const { postByUser, userFromParams } = data;
-
-  let header = [
-    'OVERVIEW',
-    'POSTS',
-    'COMMENTS',
-    'HISTORY',
-    'SAVED',
-    'HIDDEN',
-    'UPVOTED',
-    'DOWNVOTED',
-    "AWARDS RECEI'VED",
-    'AWARDS GIVEN',
-  ];
 </script>
 
 <section>
-  <div class="bg-white py-3">
-    <div class="container max-w-screen-lg">
-      <ul class="flex justify-between">
-        {#each header as title}
-          <li>
-            <a href={`#`}>
-              <span class="text-[14px] font-normal hover:text-secondary-500"
-                >{title}</span
-              >
-            </a>
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </div>
   <div class="container max-w-5xl py-6">
     <div class="grid grid-cols-3 gap-4">
       <div class="col-span-2">
