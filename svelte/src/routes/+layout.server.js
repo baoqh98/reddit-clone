aae0b269c60430b57a328b1e31e028bd38097d5d
Quoc Bao <hoquocbao@protonmail.com>
2023-08-01 19:12:52 +0700

    Redirect after logou

--- svelte/src/routes/+layout.server.js
+++ svelte/src/routes/+layout.server.js
@@ -1,17 +1,15 @@
-/** @type {import('./$types').LayoutLoad} */
 /** @type {import('./$types').LayoutServerLoad} */
 
 import { redirect } from '@sveltejs/kit';
 
-export function load({ request, locals, cookies, params }) {
+export async function load({ request, locals, cookies, params }) {
   if (params.slug === 'logout') {
     cookies.delete('reddit_clone_jwt', {
       path: '/',
     });
 
-    throw redirect(307, '/');
+    throw redirect(302, '/auth/login');
   }
-
   return {
     user: locals.user,
   };
